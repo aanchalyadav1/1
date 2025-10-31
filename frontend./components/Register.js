8d08b2d82cdcb11d88045032487886a6201868da
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-10-31 13:34:42 +0530

    Delete frontend./components directory

--- frontend./components/Register.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import api from '../api';
-
-function Register() {
-  const { t } = useTranslation();
-  const [form, setForm] =
